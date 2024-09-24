33300438c62eadc9960a7407e8ef65a4785761ae
Miro <miroslaw.michna@gmail.com>
2024-09-24 23:09:43 +0200

    delete cache and artifacts folder on deploy.js execution

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,18 @@
 const hre = require("hardhat");
+const fs = require('fs');
+const path = require('path');
+
+const cacheFolder = path.join(__dirname, '..', 'cache');
+const artifactsFolder = path.join(__dirname, '..', 'artifacts');
+
 
 async function main() {
-   [owner] = await ethers.getSigners();
+    // Delete cache and artifacts
+    deleteFolder(cacheFolder);
+    deleteFolder(artifactsFolder);
+    console.log("Deleted cache and artifacts");
+
+    [owner] = await ethers.getSigners();
    console.log(`Owner: ${owner.address}`);
   const contractName = 'ArbV2';
   await hre.run("compile");
@@ -10,6 +21,24 @@ async function main() {
   await contract.deployed();
   console.log(`${contractName} deployed to: ${contract.address}`); 
   console.log('Put the above contract address into the .env file under arbContract');
+}
+
+function deleteFolder(folderPath) {
+  if (fs.existsSync(folderPath)) {
+    fs.readdirSync(folderPath).forEach((file) => {
+      const curPath = path.join(folderPath, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        // Rekursiv Unterordner löschen
+        deleteFolder(curPath);
+      } else {
+        // Datei löschen
+        fs.unlinkSync(curPath);
+      }
+    });
+    // Leeren Ordner löschen
+    fs.rmdirSync(folderPath);
+    //console.log(`Deleted folder: ${folderPath}`);
+  }
 }
 
 main()
